21b1be8c4d151191609bfccf1eee55c0b6fa4eee
evelynfoy <evelyn.foy@hotmail.com>
2022-02-18 07:52:40 +0000

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,32 @@
  */
 
 // Vanilla JavaScript Function
+/* function addTwoNumbers(a, b) {
+    return a + b
+}
+
+let sum = addTwoNumbers(4,5);
+console.log(sum); */
 
 // Arrow Function With Parameters
 
+const addTwoNumbers = (a, b) => {
+    return a + b
+}
+
+let sum = addTwoNumbers(4,5);
+console.log(sum);
+
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) =>  a + b
+let sum2 = addTwoNumbers2(4,5);
+console.log(sum2);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello');
+
+const sayHello = () => console.log('Hello');
+sayHello();
 
 // Returning Multiple Lines
